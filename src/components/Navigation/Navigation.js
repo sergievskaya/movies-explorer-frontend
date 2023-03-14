1ac6601cb70c673378635fb126a5161531647387
Ekaterina Sergievskaya <sergievskaya_ea@mail.ru>
2023-03-14 16:49:01 +0700

    feat: добавить ссылки в header

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Navigation.css';
 
-function Navigation() {
+function Navigation({ pathname }) {
+  
     return(
-        <nav className="navigation">
-
-        </nav>
+        <>
+            {pathname === '/' ? (
+                <nav className="navigation">
+                    <ul className="navigation__list">
+                        <li className="navigation__list-item">
+                            <Link to="/signup" className="navigation__link">Регистрация</Link>
+                        </li>
+                        <li className="navigation__list-item">
+                            <Link to="/signin" className="navigation__link navigation__link_type_signin">Войти</Link>
+                        </li>
+                    </ul>  
+                </nav>
+            ) : (
+                <> </>
+            )}
+        </>
     );
 }
 
