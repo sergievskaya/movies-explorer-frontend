f28c441dc9faf6b5f3c02a8bd7a00ac0ca84cde7
Ekaterina Sergievskaya <sergievskaya_ea@mail.ru>
2023-03-19 17:34:32 +0700

    feat: добавить свою фотографию

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,8 +8,8 @@ function AboutMe() {
             <h2 className="about-me__title">Студент</h2>
             <div className="about-me__container">
                 <div className="about-me__info-container">
-                    <h3 className="about-me__name">Виталий</h3>
-                    <p className="about-me__job">Фронтенд-разработчик, 30 лет</p>
+                    <h3 className="about-me__name">Екатерина</h3>
+                    <p className="about-me__job">Фронтенд-разработчик, 20 лет</p>
                     <p className="about-me__description">
                     Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена 
                     и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал 
